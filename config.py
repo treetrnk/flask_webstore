9ab8a0c556e19722c78239bd276ed9a5e97f24b0
Nathan Hare <nhare330@gmail.com>
2020-09-05 06:52:27 -0400

    Add low availability threshold config setting

--- config.py
+++ config.py
@@ -50,3 +50,4 @@ class Config(object):
     STRIPE_PUBLISHABLE = os.environ.get('STRIPE_PUBLISHABLE') or ''
     STRIPE_SECRET = os.environ.get('STRIPE_SECRET') or ''
     STRIPE_WEBHOOK = os.environ.get('STRIPE_WEBHOOK') or ''
+    LOW_AVAILABILITY_THRESHOLD = os.environ.get('LOW_AVAILABILITY_THRESHOLD') or 3
